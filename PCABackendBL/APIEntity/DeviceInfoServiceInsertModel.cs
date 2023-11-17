27732a2a1a37ca1ff6c946a134a1e5911f586ca4
Sakunthala Jayasundara <Sakunthala.Jayasundara@inivosglobal.com>
2023-11-17 11:06:16 +0530

    Removed created date from user and device registrations

---
+++ PCABackendBL/APIEntity/DeviceInfoServiceInsertModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCABackendBL.APIEntity
+{
+    public class DeviceInfoServiceInsertModel
+    {
+        public int DeviceId { get; set; }
+        public string DeviceSerialKey { get; set; }
+        public string DeviceType { get; set; }
+        public string ApplianceName { get; set; }
+        public string InternalLocation { get; set; }
+        public string Address { get; set; }
+        public string UserName { get; set; }
+        public string UserCode { get; set; }
+        public float PowerThresholdValue { get; set; } 
+
+    }
+}
